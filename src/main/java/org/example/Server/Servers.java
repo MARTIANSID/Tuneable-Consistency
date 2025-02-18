98172e7f8f5499e6eabc4403af8e89a5c82ad6b8
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2025-02-17 19:29:58 -0500

    changes

--- src/main/java/org/example/Server/Servers.java
+++ src/main/java/org/example/Server/Servers.java
@@ -2,7 +2,6 @@ package org.example.Server;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
-import org.example.Keys.KeyGeneration;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -20,36 +19,10 @@ public class Servers{
     public static void main(String[] args) throws IOException, InterruptedException {
 
         List<Server> servers = new ArrayList<>();
-
-
-
-        // cluster 1
-        for (int i = 1; i <= 4; i++) {
-            int port = 8000 + i; // Ports 8000 to 8004
-            Server server = ServerBuilder.forPort(port)
-                    .addService(new ServerImpl(("S"+i),1,connection, i))
-                    .build()
-                    .start();
-            System.out.println("Server" + (i + 1) + " started on port " + port);
-            servers.add(server);
-        }
-
-        // cluster 2
         for (int i = 5; i <= 8; i++) {
             int port = 8000 + i; // Ports 8000 to 8004
             Server server = ServerBuilder.forPort(port)
-                    .addService(new ServerImpl(("S"+i),2,connection, i))
-                    .build()
-                    .start();
-            System.out.println("Server" + (i + 1) + " started on port " + port);
-            servers.add(server);
-        }
-//
-        // cluster 3
-        for (int i = 9; i <= 12; i++) {
-            int port = 8000 + i; // Ports 8000 to 8004
-            Server server = ServerBuilder.forPort(port)
-                    .addService(new ServerImpl(("S"+i),3,connection, i))
+                    .addService(new ServerImpl())
                     .build()
                     .start();
             System.out.println("Server" + (i + 1) + " started on port " + port);
