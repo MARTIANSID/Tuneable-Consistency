37f7546a2cf4ca5b496f85c54083ed7eedb869b2
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2025-02-18 12:23:23 -0500

    request vote implemented

--- src/main/java/org/example/Server/ServerImpl.java
+++ src/main/java/org/example/Server/ServerImpl.java
@@ -26,7 +26,9 @@ import org.example.Utility.ServerStatus.*;
 
 public class ServerImpl extends RaftGrpc.RaftImplBase {
     AtomicInteger currentTerm;
-    AtomicInteger votedFor;
+
+
+    ConcurrentHashMap<Integer, Integer> votedFor; // term : candidateId
     ConcurrentLinkedDeque<Log> log;
 
     AtomicInteger commitIndex;
@@ -44,9 +46,13 @@ public class ServerImpl extends RaftGrpc.RaftImplBase {
     AtomicInteger votes;
 
     ServerCurrentStatus status;
+
+    AtomicBoolean isElectionOver;
+
+    int currentLeader;
     public ServerImpl(int serverId) {
         this.currentTerm = new AtomicInteger(0);
-        this.votedFor = new AtomicInteger(-1);
+        this.votedFor = new ConcurrentHashMap<>();
         this.log = new ConcurrentLinkedDeque<>();
         this.commitIndex = new AtomicInteger(-1);
         this.lastApplied = new AtomicInteger(-1);
@@ -58,9 +64,7 @@ public class ServerImpl extends RaftGrpc.RaftImplBase {
         this.stubs = new RaftStub[5];
         this.status = ServerCurrentStatus.FOLLOWER;
         this.votes = new AtomicInteger(0);
-
-
-
+        this.isElectionOver = new AtomicBoolean(false);
 
 
         // setting the peers list
@@ -83,9 +87,31 @@ public class ServerImpl extends RaftGrpc.RaftImplBase {
     public void appendEntries(AppendEntriesArgument request, StreamObserver<AppendEntriesResult> responseObserver) {
     }
 
+    private boolean isUpToDateCandidateLog(int lastLogTermOfCandidate, int lastLogIndexOfCandidate) {
+        int lastLogTermOfCurrentNode = getLastLogTerm(), lastLogIndexOfCurrentNode = getLastLogTerm();
+
+        // deny vote condition
+        if((lastLogTermOfCurrentNode > lastLogTermOfCandidate) || ((lastLogTermOfCurrentNode == lastLogTermOfCandidate) && (lastLogIndexOfCurrentNode > lastLogIndexOfCandidate))) {
+            return false;
+        }
+
+        return true;
+    }
+
     @Override
-    public void requestVote(RequestVoteArguments request, StreamObserver<RequestVoteResult> responseObserver) {
-        super.requestVote(request, responseObserver);
+    public void requestVote(RequestVoteArguments requestVoteArguments, StreamObserver<RequestVoteResult> responseObserver) {
+        int currentTermOfTheCandidate = requestVoteArguments.getCandidatesTerm(), lastLogIndexOfCandidate = requestVoteArguments.getLastLogIndex(), lastLogTermOfCandidate = requestVoteArguments.getLastLogIndex();
+
+        boolean isVoteGranted = true;
+
+        if(votedFor.containsKey(this.currentTerm) || (this.currentTerm.get() > currentTermOfTheCandidate) || !isUpToDateCandidateLog(lastLogTermOfCandidate, lastLogIndexOfCandidate)) {
+            // reply false here
+            isVoteGranted = false;
+        }
+
+        RequestVoteResult requestVoteResult = RequestVoteResult.newBuilder().setIsVoteGranted(isVoteGranted).setCurrentTerm(currentTerm.get()).build();
+        responseObserver.onNext(requestVoteResult);
+        responseObserver.onCompleted();
     }
 
     @Override
@@ -118,15 +144,21 @@ public class ServerImpl extends RaftGrpc.RaftImplBase {
        return RequestVoteArguments.newBuilder().setCandidateId(this.serverId).setCandidatesTerm(this.currentTerm.get()).setLastLogTerm(this.getLastLogTerm()).setLastLogIndex(this.getLastLogIndex()).build();
     }
 
-    private void handleRequestVoteResult(RequestVoteResult requestVoteResult) {
+    private boolean handleRequestVoteResult(RequestVoteResult requestVoteResult) {
         if(requestVoteResult.getCurrentTerm() > currentTerm.get()) {
             // this node is not upto date
             currentTerm.set(requestVoteResult.getCurrentTerm());
+            // now it cannot become the leader
+            return false;
         } else if(requestVoteResult.getIsVoteGranted()) {
             // vote granted
             int votesTillNow = votes.get();
             votes.set(votesTillNow + 1);
+
+            // voite granted
+            return true;
         }
+        return true;
     }
 
     private void requestForVotes() {
@@ -138,10 +170,21 @@ public class ServerImpl extends RaftGrpc.RaftImplBase {
            stubs[i].requestVote(requestVoteArguments, new StreamObserver<RequestVoteResult>() {
                @Override
                public void onNext(RequestVoteResult requestVoteResult) {
-                    handleRequestVoteResult(requestVoteResult);
-                    latch.countDown();
-               }
+                   if(isElectionOver.get()) {
+                       return;
+                   }
+                    boolean isSuccessful = handleRequestVoteResult(requestVoteResult);
 
+                    // it is not successful when the currentTerm of the leader is not upto date
+                    if(!isSuccessful) {
+                        votes.set(-(int)1e9);
+                        while(latch.getCount() > 0) {
+                            latch.countDown();
+                        }
+                    } else {
+                        latch.countDown();
+                    }
+               }
                @Override
                public void onError(Throwable throwable) {
 
@@ -157,26 +200,31 @@ public class ServerImpl extends RaftGrpc.RaftImplBase {
         try {
             // Wait for up to 30ms for responses
             boolean success = latch.await(30, TimeUnit.MILLISECONDS);
+            // now election is over cannot receive more responses
+            isElectionOver.set(true);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
     }
 
+    private void sendAppendEntries(boolean isHeartBeat) {
+        for(int i = 0; i < 5; i++) {
+
+        }
+    }
     private void sendAppendEntries() {
         // send appendEntries
 
         for(int i = 0; i < 5; i++) {
-
             // this is the condtion that we need to just sendHeartBeat
             if ((log.size() == 0) || (matchIndex.get(i) == log.size() - 1)) {
-                
+                sendAppendEntries(true);
             } else {
-
+                sendAppendEntries(false);
             }
         }
 
     }
-
     public void reinitialiseIndexes() {
         for(int i = 0; i < 5; i++) {
             nextIndex.set(i, 0);
@@ -187,21 +235,25 @@ public class ServerImpl extends RaftGrpc.RaftImplBase {
     public void startElection() {
         // Starting Election
         System.out.println("Starting Election");
+        // this node becomes a candidate
+        this.status = ServerCurrentStatus.CANDIDATE;
         // first update the term
         updateTerm();
         // resetting the votes
         votes.set(0);
+        isElectionOver.set(false);
         requestForVotes();
 
         if(votes.get() >= 2) {
             // reinitialise state
             reinitialiseIndexes();
-
             // this node becomes the leader
             this.status = ServerCurrentStatus.LEADER;
+            this.currentLeader = this.serverId;
             // start sending AppendEntries
             sendAppendEntries();
         } else {
+            this.status = ServerCurrentStatus.FOLLOWER;
             startTheElectionTimer();
         }
     }
